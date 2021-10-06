d3db78d59fcf228931e07b9e7c6358d930459aae
nezam0003 <fhameedk.nezam@gmail.com>
2021-10-06 21:54:44 +0600

    model and schema design

---
+++ 04-Store-API/models/products.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "price is required"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAT: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
+    },
+    // enum:['ikea','liddy', 'caressa', 'marcos']
+  },
+});
+
+/***** export module with model */
+module.exports = mongoose.model("Product", productSchema);
