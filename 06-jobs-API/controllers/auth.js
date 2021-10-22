4fb6c128c33ddf65650d153661fa854d3ad49911
nezam0003 <fhameedk.nezam@gmail.com>
2021-10-22 19:57:08 +0600

    Login basic setup

--- 06-jobs-API/controllers/auth.js
+++ 06-jobs-API/controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnAuthenticationError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
@@ -9,7 +9,24 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnAuthenticationError("Invalid credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticationError("Invalid credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
