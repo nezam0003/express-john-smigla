97f9da42fda86a2a06c9c0f1d0a624b792893ce6
nezam0003 <fhameedk.nezam@gmail.com>
2021-10-27 15:48:16 +0600

    createdJob done

--- 06-jobs-API/controllers/jobs.js
+++ 06-jobs-API/controllers/jobs.js
@@ -1,12 +1,20 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
   res.send("get All jobs");
 };
 const getJob = async (req, res) => {
   res.send("get a job");
 };
+
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send("update a job");
 };
