de4fa55cfca3c53c1a82deecd24f77701fdf78d1
nezam0003 <fhameedk.nezam@gmail.com>
2021-10-30 21:18:08 +0600

    implement extra security packages

--- 06-jobs-API/app.js
+++ 06-jobs-API/app.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 require("express-async-errors");
-const express = require("express");
 
+// Extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
@@ -15,6 +21,18 @@ const errorHandlerMiddleware = require("./middlewares/error-handler");
 
 /***** Built-in Middlewares */
 app.use(express.json());
+
+/****** external middlewares */
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15mins
+    max: 100, // Maximum request accepts
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 /******* Routes */
 app.use("/api/v1/auth", authRouter);
